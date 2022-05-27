1bb37df9504082d28012628a8f849b0e8d4a808c
NoahSchiro <noahschiro@rocketmail.com>
2022-05-26 22:53:05 -0400

    client side network programming should be done but cannot be tested until server side is up

--- software/client/main.c
+++ software/client/main.c
@@ -1,12 +1,17 @@
 #include "network.h"
 #include "lightbulb.h"
 
+static const char*    server_ip   = "192.0.0.1";
+static const uint16_t server_port = 6969;
+
 int main() {
 
 	lbCommand(255, 255, 255);
 	lbCommand(255, 255, 0);
 	lbCommand(255, 0, 0);
 	lbCommand(0, 0, 0);
+
+	netSetup(server_ip, server_port);
 
 	return 0;
 }
--- software/client/network.h
+++ software/client/network.h
@@ -1,8 +1,26 @@
 #ifndef NETWORK_H
 #define NETWORK_H
 
-void netSetup();
-void connect();
+#include <stdio.h>			//For networking
+#include <unistd.h>			//For read networking function
+#include <stdint.h>			//For uint types
+#include <sys/socket.h>		//For socket creation
+#include <sys/types.h>		//For socket creation
+#include <arpa/inet.h>		//For networking
+#include <errno.h>			//Error reporting
+#include <err.h>			//Error reporting
+#include <string.h>			//Error reporting
+
+//Sets up connection to the server 
+//provided an IP and port number
+int netSetup(const char* ip, uint16_t port);
+
+//Reads data from the server
+void netRead();
+
+//Destroys the connection to the
+//server and properly handles memory
+//Returns success or failure
 void netShutdown();
 
 #endif
